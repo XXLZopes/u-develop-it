644176e6eac674a2c613cdf15627443bc683f6ec
XXLZopes <afordm99@gmail.com>
2021-01-13 14:51:52 -0800

    created put request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck')
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -26,6 +27,52 @@ db.on("open", () => {
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World",
+  });
+});
+
+//Get all parties
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if(err) {
+      res.status(500).json({ err: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if(err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+    
+    res.json({ message: 'successfully deleted', changes: this.changes });
   });
 });
 
@@ -100,6 +147,32 @@ app.delete("/api/candidates/:id", (req, res) => {
 //     }
 //     console.log(result, this.lastID);
 // });
+
+//Change candidates party
+app.put('/api/candidate/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
+
+  if(errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `UPDATE candidates SET party_id = ?
+              WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.run(sql, params, function(err, result) {
+    if(err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: req.body,
+      changes: this.changes
+    });
+  });
+});
 
 //Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
